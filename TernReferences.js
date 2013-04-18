1a0415291116b32f123134449fd3d72623d927a4
Miguel Castillo <miguel.castillo@borland.com>
2013-04-18 13:17:45 -0400

    Added a separate function to handle reference replacement

--- TernReferences.js
+++ TernReferences.js
@@ -36,10 +36,67 @@ define(function (require, exports, module) {
     }
 
 
+    TernReferences.prototype.query = function( cm ) {
+        var _self = this;
+
+        return _self.ternProvider.query( cm, "refs" )
+            .pipe(function(data) {
+                var perFile = {}, i, use;
+
+                for (i = 0; i < data.refs.length; ++i) {
+                    use = data.refs[i];
+                    (perFile[use.file] || (perFile[use.file] = [])).push(use);
+                }
+
+                return perFile;
+            },
+            function(error) {
+                return error;
+            });
+    };
+
+
+    /**
+    * Finds and highlights all refenrences of the token the cursor is currently sitting on
+    */
     TernReferences.prototype.findReferences = function( cm ) {
         var _self = this;
 
-        $.each( _self.textMarkers.slice(0), function(i1, textMarker) {
+        // Clear markers
+        $.each( _self.textMarkers.slice(0), function(index, textMarker) {
+            textMarker.clear();
+        });
+
+        _self.textMarkers = [];
+
+        if ( !cm ) {
+            return $.Deferred().reject("No codemirror instance was provided.");
+        }
+
+        _self.query(cm).done(function(refsPerFile) {
+            var i = 0;
+
+            for (var file in refsPerFile) {
+                var refs = refsPerFile[file], doc = _self.ternProvider.findDocByName(file).doc;
+                refs.sort(refSort);
+
+                for (i = 0; i < refs.length; ++i) {
+                    var marker = cm.markText(refs[i].start, refs[i].end, {className: "Tern-reference-highlight"});
+                    _self.textMarkers.push(marker);
+                }
+            }
+        });
+    };
+
+
+    /**
+    * Finds and highlights all refenrences of the token the cursor is currently sitting on,
+    * then allows to replace all those highlighted references
+    */
+    TernReferences.prototype.replaceReferences = function( cm ) {
+        var _self = this;
+
+        $.each( _self.textMarkers.slice(0), function(index, textMarker) {
             textMarker.clear();
         });
 
@@ -50,31 +107,24 @@ define(function (require, exports, module) {
             return $.Deferred().reject();
         }
 
-        _self.ternProvider.query( cm, "refs" )
-            .pipe(function(data) {
-                var perFile = {}, i;
+        if ( !cm ) {
+            return $.Deferred().reject("No codemirror instance was provided.");
+        }
 
-                for (i = 0; i < data.refs.length; ++i) {
-                    var use = data.refs[i];
-                    (perFile[use.file] || (perFile[use.file] = [])).push(use);
+        _self.query(cm).done(function(refsPerFile) {
+            var i = 0;
+
+            for (var file in refsPerFile) {
+                var refs = refsPerFile[file], doc = _self.ternProvider.findDocByName(file).doc;
+                refs.sort(refSort);
+
+                for (i = 0; i < refs.length; ++i) {
+                    //doc.replaceRange(newName, doc.posFromIndex(refs[i].start), doc.posFromIndex(refs[i].end));
+                    var marker = cm.markText(refs[i].start, refs[i].end, {className: "Tern-reference-highlight"});
+                    _self.textMarkers.push(marker);
                 }
-
-                for (var file in perFile) {
-                    var refs = perFile[file], doc = _self.ternProvider.findDocByName(file).doc;
-                    refs.sort(refSort);
-
-                    for (i = 0; i < refs.length; ++i) {
-                        //doc.replaceRange(newName, doc.posFromIndex(refs[i].start), doc.posFromIndex(refs[i].end));
-                        var marker = cm.markText(refs[i].start, refs[i].end, {className: "Tern-reference-highlight"});
-                        _self.textMarkers.push(marker);
-                    }
-                }
-
-                return perFile;
-            },
-            function(error) {
-                return error;
-            });
+            }
+        });
     };
 
 
